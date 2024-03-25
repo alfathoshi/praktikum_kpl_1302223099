0fdbc75b655cb5cb560550888c24c8e91e96a1f2
unknown <alfathbintangmuhammad@gmail.com>
2024-03-25 07:12:21 +0700

    menambahkan dua Class SayaTubeVideo dan SayaTubeUser

--- modul6_1302223099/Program.cs
+++ modul6_1302223099/Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using modul6_1302223099;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SayaTubeUser user = new SayaTubeUser("Bintang");
+        SayaTubeVideo[] video =
+        {
+            new SayaTubeVideo("Avengers: Infinity War"),
+            new SayaTubeVideo("SpiderMan: No Way Home"),
+            new SayaTubeVideo("Your Name"),
+            new SayaTubeVideo("Silent Voice"),
+            new SayaTubeVideo("Exhuma"),
+            new SayaTubeVideo("The Greatest Showman"),
+            new SayaTubeVideo("Hachiko: The Dog Story"),
+            new SayaTubeVideo("Ready Player One"),
+            new SayaTubeVideo("Top Gun: Maverick"),
+            new SayaTubeVideo("Weathering With You"),
+        };
+
+        for (int i = 0; i < video.Length; i++)
+        {
+            user.AddVideo(video[i]);
+        }
+        user.PrintAllVideoPlayCount();
     }
 }
---
+++ modul6_1302223099/SayaTubeVideo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302223099
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+        public SayaTubeVideo(string title)
+        {
+            var rand = new Random();
+            this.title = title;
+            id = rand.Next(10000, 99999);
+            playCount = 0;
+        }
+        public void IncreasePlayCount(int play)
+        {
+            playCount += play;
+        }
+        public string GetTitle()
+        {
+            return title;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID         : {id}");
+            Console.WriteLine($"Title      : {title}");
+            Console.WriteLine($"Play Count : {playCount}");
+        }
+    }
+}
